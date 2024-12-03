5db09daa8984af8fce75fcaea73fcc1404a197ae
malikhan-dev <109470179+malikhan-dev@users.noreply.github.com>
2024-12-02 22:14:24 -0800

    init

---
+++ samples/EndPoints/Base.Samples.EndPoints.WebApi/Validator/FluentValidationExtenstion.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+
+namespace Base.Samples.EndPoints.WebApi.Validator
+{
+    public class FluentValidationExtenstion
+    {
+        public static void Init(IServiceCollection services, Type InitType)
+        {
+            services.AddValidatorsFromAssemblyContaining(InitType);
+        }
+
+        public static void RegisterValidator<TModel, TValidator>(IServiceCollection services) where TValidator : AbstractValidator<TModel>
+        {
+            services.AddScoped<IValidator<TModel>, TValidator>();
+
+        }
+    }
+}
